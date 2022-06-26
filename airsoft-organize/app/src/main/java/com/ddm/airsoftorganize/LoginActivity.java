a549af33fb69a9bbb3bf18bda16601d197490ede
André Cristen <30880513+andrecristen@users.noreply.github.com>
2022-06-26 11:47:26 -0300

    Mensagens de sucesso e erro ao efetuar login

--- airsoft-organize/app/src/main/java/com/ddm/airsoftorganize/LoginActivity.java
+++ airsoft-organize/app/src/main/java/com/ddm/airsoftorganize/LoginActivity.java
@@ -2,11 +2,14 @@ package com.ddm.airsoftorganize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ddm.airsoftorganize.models.AuthResponse;
@@ -40,24 +43,37 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ProgressDialog progress = new ProgressDialog(context);
+                progress.setTitle("Carregando..");
+                progress.setMessage("Aguarde a validação de login...");
+                progress.setCancelable(false);
+                progress.show();
+                EditText login = findViewById(R.id.loginUsername);
+                EditText password = findViewById(R.id.loginPassword);
                 RequestBody requestBody = new MultipartBody.Builder()
                         .setType(MultipartBody.FORM)
-                        .addFormDataPart("login", "teste@airsoftorganize.com")
-                        .addFormDataPart("password", "12345")
+                        .addFormDataPart("login", login.getText().toString())
+                        .addFormDataPart("password", password.getText().toString())
                         .build();
                 Call<AuthResponse> call = new RetrofitInitializer().auth().executeAuth(requestBody);
                 call.enqueue(new Callback<AuthResponse>() {
                     @Override
                     public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
-                        if (response.body().getSuccess().equals("true") && !response.body().getToken().isEmpty()) {
-                            UserSession userSession = UserSession.getInstance(response.body().getToken());
-                            Toast.makeText(context, "Login efetuado com sucesso", Toast.LENGTH_LONG).show();
+                        progress.dismiss();
+                        if (response.body() != null) {
+                            if (response.body().getSuccess().equals("true") && !response.body().getToken().isEmpty()) {
+                                UserSession userSession = UserSession.getInstance(response.body().getToken());
+                                Toast.makeText(context, "Login efetuado com sucesso", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(context, "Erro ao efetuar login: " + response.body().getMessage(), Toast.LENGTH_LONG).show();
+                            }
                         } else {
-                            Toast.makeText(context, "Erro ao efetuar login: " + response.body().getMessage(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(context, "Erro ao efetuar login, verifique os dados e tente novamente.", Toast.LENGTH_LONG).show();
                         }
                     }
                     @Override
                     public void onFailure(Call<AuthResponse> call, Throwable t) {
+                        progress.dismiss();
                         Toast.makeText(context, "Falha na requisição", Toast.LENGTH_LONG).show();
                     }
                 });
